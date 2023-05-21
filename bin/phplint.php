e957bc83386ebdce22d5a3f41ee83fa4172db6a3
andreas.wunderwald <wundii@gmail.com>
2023-05-21 19:19:53 +0200

    implementation of the application and config files

--- bin/phplint.php
+++ bin/phplint.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use PHPLint\Bootstrap\BootstrapConfigRequirer;
+use PHPLint\Bootstrap\BootstrapConfigResolver;
 use PHPLint\Console\Application;
 
 @ini_set('memory_limit', '-1');
@@ -10,7 +12,18 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'stderr');
 gc_disable();
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once getcwd() . '/vendor/autoload.php';
 
-$application = new Application();
-exit($application->run());
+$configResolver = new BootstrapConfigResolver();
+
+try {
+    $bootstrapConfig = $configResolver->getBootstrapConfig();
+    $bootstrapConfigRequirer = new BootstrapConfigRequirer($bootstrapConfig);
+    $lintConfig = $bootstrapConfigRequirer->getLintConfig();
+
+    $application = new Application($lintConfig);
+    exit($application->run());
+} catch (Exception $e) {
+    fwrite(STDERR, $e->getMessage() . PHP_EOL);
+    exit(1);
+}
